fe22397114a6e7df03e04297bdb63c1d7fdf5d56
Ismail Ali <ismailali07863@gmail.com>
2024-04-26 17:54:04 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,32 @@
-import { Route, Routes } from 'react-router-dom'
-import { Suspense, lazy } from 'react'; // Import Suspense and lazy
+import { Route, Routes, Navigate } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext'
 import { Toaster } from 'react-hot-toast'
 import ProtectedRoute from './Routes/ProtectedRoute'
 import PageNotFound from './Pages/PageNotFound'
-import { useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-
-const Home = lazy(() => import('./Pages/Home'));
-const Signup = lazy(() => import('./Pages/Signup'));
-const Destination = lazy(() => import('./Pages/Destination'));
-const UNESCO = lazy(() => import('./Pages/UNESCO'));
-const News = lazy(() => import('./Pages/News'));
-const SignIn = lazy(() => import('./Pages/SignIn'));
+import Signup from './Pages/Signup'
+import SignIn from './Pages/SignIn';
+import Home from './Pages/Home';
+import Destination from './Pages/Destination';
+import UNESCO from './Pages/UNESCO';
+import News from './Pages/News';
 
 const App = () => {
-  const { pathname } = useLocation();
-  
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  }, [pathname]);
-
   return (
     <>
       <Toaster />
       <AuthContextProvider>
-        <Suspense fallback={<div>Loading...</div>}> {/* Add Suspense with fallback */}
-          <Routes>
-            <Route index path='/signin' element={<SignIn/>} exact/>
-            <Route path='/signup' element={<Signup/>} exact/>
-            <Route element={<ProtectedRoute/>}>
-              <Route element={<Home/>} path='/' exact/>
-              <Route element={<Destination/>} path='/destinations' exact/>
-              <Route element={<UNESCO/>} path='/unesco-site' exact/>
-              <Route element={<News/>} path='/news' exact/>
-              <Route path='*' element={<PageNotFound/>}/>
-            </Route>
+        <Routes>
+          <Route path='/signin' element={<SignIn/>} exact/>
+          <Route path='/signup' element={<Signup/>} exact/>
+          <Route element={<ProtectedRoute/>}>
+            <Route element={<Home/>} path='/' exact/>
+            <Route element={<Destination/>} path='/destinations' exact/>
+            <Route element={<UNESCO/>} path='/unesco-site' exact/>
+            <Route element={<News/>} path='/news' exact/>
             <Route path='*' element={<PageNotFound/>}/>
-          </Routes>
-        </Suspense>
+          </Route>
+          <Route path='*' element={<Navigate to="/"/>}/>
+        </Routes>
       </AuthContextProvider>
     </>
   )
